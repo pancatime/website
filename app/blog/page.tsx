af333f6ec86ba9c6609128ba865daebc6aecb01d
pancatime <pancaxd@proton.me>
2025-10-06 22:16:04 +0200

    Epic blog list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,22 +6,24 @@ export default async function Blog() {
   const blogs = await Blogs.findAll()
 
   return (
-    <div className="max-w-[720px] m-auto p-4 box-border">
-      <header className="mt-80 ">
-        <Navigation navItems={[ { href: "/", title: "< Blog" }, ]}/>
-      </header>
-      <main className="mt-20">
-        <ul>
-          {blogs.map((blog, i) => (
-            <li key={i}>
-              <Link href={"blog/" + blog.slug}>
-                <p className="text-2xl font-bold my-4 transition duration-300 hover:text-white">
-                  {`${blog.postedAt.getDate()}.${blog.postedAt.getMonth() + 1}.${blog.postedAt.getFullYear()} ${blog.title}`}
-                </p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <div className="grid content-center h-full">
+      <main className="flex">
+        <div className="border-l border-r p-4 box-border w-xl h-64">
+          <ul>
+            {blogs.map((blog, i) => (
+              <li key={i}>
+                <Link href={"blog/" + blog.slug}>
+                  <p className="text-2xl font-bold transition duration-300 hover:text-white">
+                    {`${blog.postedAt.getDate()}.${blog.postedAt.getMonth() + 1}.${blog.postedAt.getFullYear()} ${blog.title}`}
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <header>
+          <h1 className="[writing-mode:vertical-rl] [text-orientation:upright]">BLOGS</h1>
+        </header>
       </main>
     </div>
   );
