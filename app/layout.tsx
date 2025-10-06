933e2dd48823190603db0c7ee36c51db17a4d2c5
pancatime <pancaxd@proton.me>
2025-10-06 21:42:10 +0200

    New layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
@@ -16,11 +17,32 @@ interface Props {
   children: Readonly<{ children: React.ReactNode; }>,
 }
 
+const links = [
+  { href: "/",      title: "Home" },
+  { href: "/blog",  title: "Blog" },
+  { href: "/about", title: "About" },
+];
+
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={`${nunito.className} bg-black text-orange-400`}>
-        {children}
+        <div className="grid grid-cols-[500px_auto]">
+          <nav className="h-screen grid place-items-center">
+            <ul className={"*:py-2 text-2xl font-bold relative w-fit h-fit"}>
+              {links.map((item) => (
+                <li key={item.href}>
+                  <Link href={item.href} className="transition duration-300 hover:text-orange-400">
+                    { item.title }
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="p-4">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,22 @@
-import Link from "next/link";
-import { Fredoka, Nunito, Passion_One } from "next/font/google";
-
-const fredoka = Fredoka({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
+import { Passion_One } from "next/font/google";
 
 const passion_one = Passion_One({
   weight: ["400", "700"],
   subsets: ["latin"]
 })
 
-const links = [
-  { href: "/blog",  title: "Blog" },
-  { href: "/about", title: "About" },
-];
-
 export default function Home() {
   return (
-    <div>
-      <div className="max-w-[720px] m-auto p-4 box-border">
-        <header className="mt-50">
-          <h1 className={`${passion_one.className} text-7xl text-orange-400`}>
-            CodingPancake
-          </h1>
-        </header>
-        <main className="my-8">
-          <nav>
-            <ul className={"*:py-2 text-2xl font-bold"}>
-              {links.map((item) => (
-                <li key={item.href}>
-                  <Link href={item.href} className="transition duration-300 hover:text-orange-400">
-                    { item.title }
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <div className="grid content-center h-full">
+      <div className="p-4 box-border h-fit w-full">
+        <main>
+          <header className="w-fit">
+            <h1 className={`${passion_one.className} text-9xl text-orange-400`}>
+              CodingPancake
+            </h1>
+            <p className="text-right">Copyright 2025 © Panca</p>
+          </header>
         </main>
-        <footer className="col-start-1 mt-24">
-          <p>Copyright 2025 © Panca</p>
-        </footer>
       </div>
     </div>
   );
