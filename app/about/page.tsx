faa2b601e0f3e1d6d68c13e49b5f88a3fff915e6
pancatime <pancaxd@proton.me>
2025-10-07 07:32:32 +0200

    Epic about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,13 @@
-import Navigation from "@/components/navigation";
-
 export default function About() {
   return (
-    <div className="max-w-[720px] m-auto p-4 box-border">
-      <header className="mt-80">
-        <Navigation navItems={[ { href: "/", title: "< About" }, ]}/>
-      </header>
-      <main className="mt-20">
-        <p className="text-xl">Hi, my name is Panca.</p>
+    <div className="grid content-center h-full">
+      <main className="w-xl">
+        <header>
+          <h1>▮ About</h1>
+        </header>
+        <p className="mt-2 text-xl">
+          Hi, welcome to CodingPancake - the website where I share findings about tech, life and upcoming projects!
+        </p>
       </main>
     </div>
   );
