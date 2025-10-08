5d3001f28fa44ad524855966a0b313db6ad993a8
pancatime <pancaxd@proton.me>
2025-10-08 19:30:46 +0200

    Fine tune blogs page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,16 +5,22 @@ import Link from "next/link";
 export default async function Blog() {
   const blogs = await Blogs.findAll()
 
+  const formatter = new Intl.DateTimeFormat('en-UK', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+  
   return (
     <div className="grid content-center h-full">
       <main className="flex">
-        <div className="border-l border-r p-4 box-border w-xl h-64">
+        <div className="border-l-2 border-r-2 p-4 box-border w-xl h-64">
           <ul>
             {blogs.map((blog, i) => (
               <li key={i}>
                 <Link href={"blog/" + blog.slug}>
                   <p className="text-2xl font-bold transition duration-300 hover:text-white">
-                    {`${blog.postedAt.getDate()}.${blog.postedAt.getMonth() + 1}.${blog.postedAt.getFullYear()} ${blog.title}`}
+                    {formatter.format(blog.postedAt) + " - " + blog.title}
                   </p>
                 </Link>
               </li>
@@ -22,7 +28,7 @@ export default async function Blog() {
           </ul>
         </div>
         <header>
-          <h1 className="[writing-mode:vertical-rl] [text-orientation:upright]">BLOGS</h1>
+          <h1 className="[writing-mode:vertical-rl] [text-orientation:upright] font-bold">BLOGS</h1>
         </header>
       </main>
     </div>
